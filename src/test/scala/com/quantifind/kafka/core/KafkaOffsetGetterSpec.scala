49f612993fd09033c9ae2a75626d917f72880ad0
geirlund <geir.andre.lund@finn.no>
2017-08-14 12:27:59 +0200

    Update to kafka 0.10.2.1 - New endpoint /group/<groupname>/topic/<topic> - see lag information for a specific group and topic

--- src/test/scala/com/quantifind/kafka/core/KafkaOffsetGetterSpec.scala
+++ src/test/scala/com/quantifind/kafka/core/KafkaOffsetGetterSpec.scala
@@ -2,12 +2,10 @@ package com.quantifind.kafka.core
 
 import com.quantifind.kafka.offsetapp.OffsetGetterArgs
 import com.quantifind.utils.ZkUtilsWrapper
-import java.nio.{BufferUnderflowException, ByteBuffer}
-
 import kafka.api.{OffsetRequest, OffsetResponse, PartitionOffsetsResponse}
 import kafka.common.{OffsetAndMetadata, OffsetMetadata, TopicAndPartition}
-import kafka.coordinator._
 import kafka.consumer.SimpleConsumer
+import kafka.coordinator._
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.TopicPartition
 import org.mockito.Matchers._
@@ -40,7 +38,7 @@ class KafkaOffsetGetterSpec extends FlatSpec with ShouldMatchers {
 
     val topicAndPartition = TopicAndPartition(testTopic, testPartition)
     val topicPartition = new TopicPartition(testTopic, testPartition)
-    val groupTopicPartition = GroupTopicPartition(testGroup, TopicAndPartition(testTopic, testPartition))
+    val groupTopicPartition = GroupTopicPartition(testGroup, new TopicPartition(testTopic, testPartition))
     val offsetAndMetadata = OffsetAndMetadata(committedOffset, "meta", System.currentTimeMillis)
 
     KafkaOffsetGetter.committedOffsetMap += (groupTopicPartition -> offsetAndMetadata)
